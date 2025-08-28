4cff4392aa162898345a9ff35d7947f78e5e44bc
Nils Werner <nils@hey.com>
2025-08-28 10:12:18 +0200

    viewport and blue fade

--- src/raytracer/raytracer/main.hpp
+++ src/raytracer/raytracer/main.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <raytracer/color.hpp>
+#include <raytracer/ray.hpp>
 #include <raytracer/vec3.hpp>
 
 #include <iostream>
--- src/raytracer/raytracer/ray.hpp
+++ src/raytracer/raytracer/ray.hpp
@@ -1,11 +1,12 @@
 #pragma once
 
+#include <raytracer/color.hpp>
 #include <raytracer/vec3.hpp>
 
 namespace raytracer {
 class ray {
   public:
-    ray() {}
+    ray() : orig(point3(0, 0, 0)), dir(point3(0, 0, 1)) {}
 
     ray(const point3 &origin, const vec3 &direction)
         : orig(origin), dir(direction) {}
@@ -19,4 +20,12 @@ class ray {
     point3 orig;
     vec3 dir;
 };
+
+color ray_color(const ray &r);
+
+color ray_color(const ray &r) {
+    vec3 unit_direction = unit_vector(r.direction());
+    auto a = 0.5 * (unit_direction.y() + 1.0);
+    return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
+}
 } // namespace raytracer
